952586e45059317699de1334c95d2ffcffbc92cd
Ted-Jo <jotd0413@gmail.com>
2020-10-27 11:35:09 +0900

    Updates

---
+++ precache-manifest.74650b133198ad1c90eb760f57c81241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b093a1086d97bf4fd9072d4fdef2692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa6065aff77c07cf659",
+    "url": "/static/css/main.04c68c97.chunk.css"
+  },
+  {
+    "revision": "970840be4c79ac4933a4",
+    "url": "/static/js/2.1dada748.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.1dada748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa6065aff77c07cf659",
+    "url": "/static/js/main.7ca1a815.chunk.js"
+  },
+  {
+    "revision": "09704b1055c04c568e3a",
+    "url": "/static/js/runtime-main.a916b9a5.js"
+  }
+]);
